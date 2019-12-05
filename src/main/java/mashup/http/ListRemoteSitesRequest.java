f07535824305d4f653db1128161aba80d5bd4905
taylor-cox <taylor@coxintl.com>
2019-12-05 16:43:12 -0500

    made everything a bit more clean

--- src/main/java/mashup/http/ListRemoteSitesRequest.java
+++ src/main/java/mashup/http/ListRemoteSitesRequest.java
@@ -1,19 +1,7 @@
 package mashup.http;
 
 public class ListRemoteSitesRequest {
-	String remoteSiteID;
-	
-	public ListRemoteSitesRequest(String remoteSiteID) {
-		this.remoteSiteID = remoteSiteID;
-	}
-	
-	public ListRemoteSitesRequest() {}
-	
-	public String getRemoteSiteID() {
-		return remoteSiteID;
-	}
-
-	public void setRemoteSiteID(String remoteSiteID) {
-		this.remoteSiteID = remoteSiteID;
+	public ListRemoteSitesRequest() {
+		
 	}
 }
--- src/main/java/mashup/http/ListVideoRequest.java
+++ src/main/java/mashup/http/ListVideoRequest.java
@@ -1,19 +1,7 @@
 package mashup.http;
 
 public class ListVideoRequest {
-	String videoID;
-	
-	public ListVideoRequest(String videoID) {
-		this.videoID = videoID;
-	}
-	
-	public ListVideoRequest() {}
-	
-	public String getVideoID() {
-		return videoID;
-	}
-
-	public void setVideoID(String videoID) {
-		this.videoID = videoID;
+	public ListVideoRequest() {
+		
 	}
 }
