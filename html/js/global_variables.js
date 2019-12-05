be7e3bcc7d9d57b3cb20e07777d591e92a8cc369
BenjaminSlattery <50343293+BenjaminSlattery@users.noreply.github.com>
2019-12-05 16:42:11 -0500

    Connected create and delete playlist to HTML

--- html/js/global_variables.js
+++ html/js/global_variables.js
@@ -1,6 +1,8 @@
 var base_url = "https://z4mjdesn38.execute-api.us-east-1.amazonaws.com/gamma/";
 var list_video_url = base_url + "listVideo";
 var list_playlist_url = base_url + "listPlaylist";
+var delete_playlist_url = base_url + "deletePlaylist";
+var create_playlist_url = base_url + "createPlaylist";
 // var get_url = "https://z4mjdesn38.execute-api.us-east-1.amazonaws.com/secure/";
 var playlistNum = 1;
 var videoNum = 1;
