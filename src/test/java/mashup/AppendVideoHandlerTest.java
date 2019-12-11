c2eea2e550ff9c69022e39a685d90ab0fb1ac9a7
BenjaminSlattery <50343293+BenjaminSlattery@users.noreply.github.com>
2019-12-11 15:15:51 -0500

    Finished test cases for append, remove, create, and delete

--- src/test/java/mashup/AppendVideoHandlerTest.java
+++ src/test/java/mashup/AppendVideoHandlerTest.java
@@ -1,6 +1,7 @@
 package mashup;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -8,8 +9,12 @@ import org.junit.Test;
 
 import com.amazonaws.services.lambda.runtime.Context;
 
+import mashup.db.VideosDAO;
 import mashup.http.AppendVideoRequest;
 import mashup.http.AppendVideoResponse;
+import mashup.http.CreatePlaylistResponse;
+import mashup.model.Playlist;
+import mashup.model.PlaylistEntry;
 
 /**
  * A simple test harness for locally invoking your Lambda function handler.
@@ -19,10 +24,50 @@ public class AppendVideoHandlerTest extends LambdaTest {
     @Test
     public void testAppendVideoHandler() {
         AppendVideoHandler handler = new AppendVideoHandler();
+        AppendVideoResponse output = null;
         Context ctx = createContext("append");
+        Playlist p = new Playlist("Append/Remove Test Playlist");
+        String v = "1";
         
-        AppendVideoRequest request = new AppendVideoRequest("5", "My Playlist");
-        AppendVideoResponse output = handler.handleRequest(request, ctx);
+        AppendVideoRequest request = new AppendVideoRequest(v, p.getId());
+        boolean doesPlaylistContainVideo = false;
+
+		try {
+			VideosDAO.videosDAO().addPlaylist(p);
+	        List<Playlist> playlists = null;
+	        List<PlaylistEntry> videos = null;
+			playlists = VideosDAO.videosDAO().getPlaylists();
+			for (int i = 0; i < playlists.size(); i++) {
+				if (playlists.get(i).getId().matches(p.getId())) {
+					videos = playlists.get(i).getEntries();
+				}
+			}
+			for (int i = 0; i < videos.size(); i++) {
+				System.out.print(videos.get(i).toString() + "\n");
+		        if (videos.get(i).getVideoID().matches(v)) doesPlaylistContainVideo = true;
+			}
+	        Assert.assertEquals(doesPlaylistContainVideo, false);
+	        
+	        output = handler.handleRequest(request, ctx);
+			
+	        doesPlaylistContainVideo = false;
+			playlists = VideosDAO.videosDAO().getPlaylists();
+			playlists = VideosDAO.videosDAO().getPlaylists();
+			for (int i = 0; i < playlists.size(); i++) {
+				if (playlists.get(i).getId().matches(p.getId())) {
+					videos = playlists.get(i).getEntries();
+				}
+			}
+			for (int i = 0; i < videos.size(); i++) {
+				System.out.print(videos.get(i).toString() + "\n");
+		        if (videos.get(i).getVideoID().matches(v)) doesPlaylistContainVideo = true;
+			}
+	        Assert.assertEquals(doesPlaylistContainVideo, true);
+	        VideosDAO.videosDAO().deletePlaylist(p.getId());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         System.out.println(output.statusCode + ", " + output.error);
     }
 }
--- src/test/java/mashup/RemoveVideoHandlerTest.java
+++ src/test/java/mashup/RemoveVideoHandlerTest.java
@@ -1,6 +1,7 @@
 package mashup;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -8,8 +9,13 @@ import org.junit.Test;
 
 import com.amazonaws.services.lambda.runtime.Context;
 
+import mashup.db.VideosDAO;
 import mashup.http.RemoveVideoRequest;
 import mashup.http.RemoveVideoResponse;
+import mashup.model.Library;
+import mashup.model.Playlist;
+import mashup.model.PlaylistEntry;
+import mashup.model.Video;
 
 /**
  * A simple test harness for locally invoking your Lambda function handler.
@@ -19,13 +25,58 @@ public class RemoveVideoHandlerTest extends LambdaTest {
     @Test
     public void testRemoveVideoHandler() {
         RemoveVideoHandler handler = new RemoveVideoHandler();
-        RemoveVideoRequest input = new RemoveVideoRequest("5", "My Playlist");
+        RemoveVideoResponse output = null;
         Context ctx = createContext("remove");
+        Playlist p = new Playlist("Append/Remove Test Playlist");
+        Library l = null;
+		try {
+			l = VideosDAO.videosDAO().getLibrary();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        Video v = l.getVideos().get(0);
+        
+        RemoveVideoRequest request = new RemoveVideoRequest(v.getID(), p.getId());
+        boolean doesPlaylistContainVideo = false;
 
-        RemoveVideoResponse resp = handler.handleRequest(input, ctx);
-        System.out.println(resp.statusCode + ", " + resp.error);
-
-        // TODO: validate output here if needed.
-//        Assert.assertEquals("Hello from Lambda!", output);
+		try {
+			VideosDAO.videosDAO().addPlaylist(p);
+			VideosDAO.videosDAO().appendVideoToPlaylist(v.getID(), p.getId());
+	        List<Playlist> playlists = null;
+	        List<PlaylistEntry> videos = null;
+			playlists = VideosDAO.videosDAO().getPlaylists();
+			for (int i = 0; i < playlists.size(); i++) {
+				if (playlists.get(i).getId().matches(p.getId())) {
+					videos = playlists.get(i).getEntries();
+				}
+			}
+			for (int i = 0; i < videos.size(); i++) {
+				System.out.print(videos.get(i).toString() + "\n");
+		        if (videos.get(i).getVideoID().matches(v.getID())) doesPlaylistContainVideo = true;
+			}
+	        Assert.assertEquals(doesPlaylistContainVideo, true);
+	        
+	        output = handler.handleRequest(request, ctx);
+			
+	        doesPlaylistContainVideo = false;
+			playlists = VideosDAO.videosDAO().getPlaylists();
+			playlists = VideosDAO.videosDAO().getPlaylists();
+			for (int i = 0; i < playlists.size(); i++) {
+				if (playlists.get(i).getId().matches(p.getId())) {
+					videos = playlists.get(i).getEntries();
+				}
+			}
+			for (int i = 0; i < videos.size(); i++) {
+				System.out.print(videos.get(i).toString() + "\n");
+		        if (videos.get(i).getVideoID().matches(v.getID())) doesPlaylistContainVideo = true;
+			}
+	        Assert.assertEquals(doesPlaylistContainVideo, false);
+	        VideosDAO.videosDAO().deletePlaylist(p.getId());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println(output.statusCode + ", " + output.error);
     }
 }
